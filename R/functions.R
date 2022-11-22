e468de6b31894896b540eb0292be6fb14b3c9dd2
Barbara Gendron <95307996+B-Gendron@users.noreply.github.com>
2022-11-22 16:09:31 +0100

    preprocessing function

--- R/functions.R
+++ R/functions.R
@@ -17,6 +17,7 @@ get_summary <- function(data){
 #' @examples
 #' # Filter on men
 #' filter_by_gender(patients, 'male')
+#'
 #' # Filter on women
 #' filter_by_gender(patients, 'female')
 filter_by_gender <- function(data, gender){
@@ -32,4 +33,30 @@ filter_by_gender <- function(data, gender){
     else {
       print("Unexpected input value. The gender should be either 'male' or 'female'.")
     }
+}
+
+#' Pre-process the `patients` dataset to assign the right type to categorical columns
+#' @param data The `patients` dataset
+#' @export
+#' @return The pre-processed `patients` dataset
+#' @details
+#' This function allows the user to pre-process the patient dataset so that the categorical
+#' columns are considered the right way. This is particularly useful for Mac users, because
+#' it may happen that RStudio on Mac does not recognized such qualitative columns as categorical.
+preprocess_data <- function(patients) {
+  patients$GENDER <- as.factor(patients$GENDER)
+  patients$LUNG_CANCER <- as.factor(patients$LUNG_CANCER)
+  patients$SMOKING <- as.factor(patients$SMOKING)
+  patients$YELLOW_FINGERS <- as.factor(patients$YELLOW_FINGERS)
+  patients$ANXIETY <- as.factor(patients$ANXIETY)
+  patients$PEER_PRESSURE <- as.factor(patients$PEER_PRESSURE)
+  patients$CHRONIC.DISEASE <- as.factor(patients$CHRONIC.DISEASE)
+  patients$FATIGUE <- as.factor(patients$FATIGUE)
+  patients$ALLERGY <- as.factor(patients$ALLERGY)
+  patients$WHEEZING <- as.factor(patients$WHEEZING)
+  patients$ALCOHOL.CONSUMING <- as.factor(patients$ALCOHOL.CONSUMING)
+  patients$COUGHING <- as.factor(patients$COUGHING)
+  patients$SHORTNESS.OF.BREATH <- as.factor(patients$SHORTNESS.OF.BREATH)
+  patients$SWALLOWING.DIFFICULTY <- as.factor(patients$SWALLOWING.DIFFICULTY)
+  patients$CHEST.PAIN <- as.factor(patients$CHEST.PAIN)
 }
