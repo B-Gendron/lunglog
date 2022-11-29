e565cc212b879a113bbefb15fb1bc841cf66b951
Barbara Gendron <95307996+B-Gendron@users.noreply.github.com>
2022-11-29 14:53:06 +0100

    update unit tests

--- tests/testthat/test-lunglog.R
+++ tests/testthat/test-lunglog.R
@@ -4,3 +4,16 @@ test_that("train test split returns datasets with coherent dimensions", {
   test <- train_test_split(patients)[[2]]
   expect_equal(dim(train)[2], dim(test)[2])
 })
+
+test_that("filter by gender contains the right number of samples on male filtering", {
+  patientsMale <- filter_by_gender(patients, 'male')
+  patientsMaleExpected <- patients |> dplyr::filter(GENDER=='M')
+  expect_equal(patientsMaleExpected, patientsMale)
+})
+
+test_that("filter by gender contains the right number of samples on female filtering", {
+  patientsFemale <- filter_by_gender(patients, 'female')
+  patientsFemaleExpected <- patients |> dplyr::filter(GENDER=='F')
+  expect_equal(patientsFemaleExpected, patientsFemale)
+})
+
